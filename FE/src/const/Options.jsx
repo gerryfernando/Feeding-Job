5a0b403fe2ee86782ae843b52eba8600cee294af
Gerry Fernando <firmanaldif3@gmail.com>
2024-08-27 17:20:37 +0700

    add const options

---
+++ FE/src/const/Options.jsx
@@ -0,0 +1,44 @@
+const OptionsJobTag = [
+  {
+    value: "Java-Spring",
+    label: "Java Spring",
+  },
+  {
+    value: "Python-Django",
+    label: "Python Django",
+  },
+  {
+    value: "Python-Flask",
+    label: "Python Flask",
+  },
+  {
+    value: "NodeJs-Express",
+    label: "NodeJs Express",
+  },
+  {
+    value: "NodeJs-Nest",
+    label: "NodeJs Nest",
+  },
+  {
+    value: ".Net-Core ",
+    label: ".Net Core ",
+  },
+  {
+    value: "Angular",
+    label: "Angular",
+  },
+  {
+    value: "Reactjs",
+    label: "Reactjs",
+  },
+  {
+    value: "React-Native",
+    label: "React Native",
+  },
+  {
+    value: "Flutter",
+    label: "Flutter",
+  },
+];
+
+export default OptionsJobTag;
