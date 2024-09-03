8023f2d218422be082a7d078d19e8bc195bdeefd
Gerry Fernando <firmanaldif3@gmail.com>
2024-09-03 09:22:40 +0700

    Push home page and fixing bug

---
+++ FE/src/pages/HomePage/components/Form/index.jsx
@@ -0,0 +1,147 @@
+import { Grid } from "@mui/material";
+import TextFieldCom from "../../../../components/TextFieldCom";
+import SelectCom from "../../../../components/SelectCom";
+
+import { useEffect } from "react";
+import OptionsJobTag from "../../../../const/Options";
+
+const FormJob = (props) => {
+  const { RHF, isViewOnly = false, formType, row } = props;
+  const {
+    formState: { errors },
+  } = RHF;
+
+  return (
+    <form id={`${formType}-Form`}>
+      <Grid spacing={2} container>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="jobName"
+            name="jobName"
+            label="Job Name"
+            helperText={errors?.jobName ? errors?.jobName?.message : ""}
+            error={!!errors?.jobName}
+            placeholder="Job Name"
+            required
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="company"
+            name="company"
+            label="Company"
+            helperText={errors?.company ? errors?.company?.message : ""}
+            error={!!errors?.company}
+            placeholder="Company"
+            required
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="location"
+            name="location"
+            label="Location"
+            helperText={errors?.location ? errors?.location?.message : ""}
+            error={!!errors?.location}
+            placeholder="Location"
+            required
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="salary"
+            name="salary"
+            label="Salary"
+            helperText={errors?.salary ? errors?.salary?.message : ""}
+            error={!!errors?.salary}
+            placeholder="Salary"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="workType"
+            name="workType"
+            label="Work Type"
+            helperText={errors?.workType ? errors?.workType?.message : ""}
+            error={!!errors?.workType}
+            placeholder="Work Type"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <SelectCom
+            RHF={RHF}
+            label="Job Tag"
+            name="jobTag"
+            required
+            size="small"
+            fullWidth
+            options={OptionsJobTag}
+            placeholder="Pilih"
+            helperText={errors.jobTag ? errors.jobTag.message : ""}
+            error={!!errors?.jobTag}
+            disabled={isViewOnly}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="benefit"
+            name="benefit"
+            label="Benefit"
+            helperText={errors?.benefit ? errors?.benefit?.message : ""}
+            error={!!errors?.benefit}
+            placeholder="Benefit"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextFieldCom
+            disabled={isViewOnly}
+            rhf={RHF}
+            id="description"
+            name="description"
+            label="Description"
+            helperText={errors?.description ? errors?.description?.message : ""}
+            error={!!errors?.description}
+            placeholder="Description"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        {/* <Grid item xs={12} md={6}>
+        <JdAutoComplete
+          rhf={RHF}
+          id="roleId"
+          label="Role"
+          name="roleId"
+          size="small"
+          fullWidth
+          placeholder="Pilih Role"
+          loading={loadingRoleOptions}
+          options={roleOptions}
+          required
+          helperText={
+            errors.roleId ? errors.roleId.message : handleHelperText("roleId")
+          }
+          error={!!errors?.roleId}
+          multiple
+        />
+      </Grid> */}
+      </Grid>
+    </form>
+  );
+};
+
+export default FormJob;
---
+++ FE/src/pages/HomePage/index.jsx
@@ -0,0 +1,420 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import TableCom from "../../components/TableCom";
+import API from "../../services/axios";
+import moment from "moment";
+import AlertCom from "../../components/AlertCom";
+import ModalCom from "../../components/ModalCom";
+import { enqueueSnackbar } from "notistack";
+import { FormProvider, useForm } from "react-hook-form";
+import FormJob from "./components/Form";
+import { FormControl, Grid, InputLabel, MenuItem, Select } from "@mui/material";
+import OptionsJobTag from "../../const/Options";
+export default function HomePage() {
+  //Const
+  const format = "DD MMMM yyyy";
+  const column = [
+    {
+      field: "no",
+      headerName: "No",
+      minWidth: 75,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "job_name",
+      headerName: "Job Name",
+      minWidth: 250,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      minWidth: 250,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "salary",
+      headerName: "Salary",
+      minWidth: 350,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      minWidth: 350,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "location",
+      headerName: "Location",
+      minWidth: 250,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "benefit",
+      headerName: "Benefit",
+      minWidth: 350,
+      flex: 1,
+      sortable: false,
+      align: "left",
+      headerAlign: "center",
+      renderCell: ({ row }) => {
+        return (
+          <ul>
+            {row.benefit?.map((val, idx) => (
+              <li key={`${idx}-benefit${val.id}`}>{val}</li>
+            ))}
+          </ul>
+        );
+      },
+    },
+    {
+      field: "job_tag",
+      headerName: "Job Tag",
+      minWidth: 150,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "publishDate",
+      headerName: "Publish Date",
+      minWidth: 300,
+      flex: 1,
+      sortable: false,
+      align: "center",
+      headerAlign: "center",
+    },
+  ];
+  const defaultValue = {
+    filter: "all",
+  };
+  const RHF = useForm();
+
+  //State
+  const [dataRow, setDataRow] = useState([]);
+  const [params, setParams] = useState(defaultValue);
+  const [loading, setLoading] = useState(true);
+  const [total, setTotal] = useState(0);
+  const [showAlertDelete, setShowAlertDelete] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
+  const [showFormModal, setShowFormModal] = useState(false);
+  const [showModalGenerateJobs, setShowModalGenerateJobs] = useState(false);
+  const [formType, setFormType] = useState("");
+  const [tempRow, setTempRow] = useState(null);
+  const [jobTagValue, setjobTagValue] = useState(null);
+
+  //Function
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const url = "/job";
+      const res = await API.get(url, {
+        params,
+      });
+      setTotal(res?.data?.total);
+      setDataRow(
+        (res?.data?.data || []).map((val, idx) => ({
+          ...val,
+          publishDate: val.publish_date
+            ? moment(val.publish_date).format(format)
+            : moment().format(format),
+          no: idx + 1,
+        }))
+      );
+    } catch (error) {
+      enqueueSnackbar("Get data failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleEdit = async (data) => {
+    try {
+      console.log(data);
+      setLoading(true);
+      const url = `/job/${data?.id}`;
+      const newData = {
+        ...data,
+        benefit: data?.benefit ? data?.benefit.split(",") : [],
+      };
+      console.log(newData, url);
+      await API.put(url, newData);
+      enqueueSnackbar("Edit data success", { variant: "success" });
+    } catch (error) {
+      enqueueSnackbar("Edit data failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+      getData();
+      handleCloseModalForm();
+    }
+  };
+
+  const handleAdd = async (data) => {
+    try {
+      setLoading(true);
+      const url = `/job`;
+      const newData = {
+        ...data,
+        benefit: data?.benefit ? data?.benefit.split(",") : [],
+      };
+      await API.post(url, newData);
+      enqueueSnackbar("Add data success", { variant: "success" });
+    } catch (error) {
+      enqueueSnackbar("Add data failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+      getData();
+      handleCloseModalForm();
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      setLoading(true);
+      const url = `/job/${id}`;
+      await API.delete(url);
+      enqueueSnackbar("Delete data success", { variant: "success" });
+    } catch (error) {
+      enqueueSnackbar("Delete data failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+      getData();
+      setShowAlertDelete(false);
+      setDeleteId(null);
+    }
+  };
+
+  const downloadFile = (res) => {
+    var data = new Blob([res], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    var excelUrl = window.URL.createObjectURL(data);
+    let tempLink = document.createElement("a");
+    tempLink.href = excelUrl;
+    tempLink.setAttribute("download", "jobs.xlsx");
+    tempLink.click();
+  };
+  const handleDownload = async () => {
+    try {
+      setLoading(true);
+      const url = "/download";
+      const res = await API.get(url, { responseType: "blob" });
+      downloadFile(res.data);
+      //   downloadFile(res);
+    } catch (error) {
+      enqueueSnackbar("Download data failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const generateJobs = async () => {
+    try {
+      setLoading(true);
+      const tag = jobTagValue;
+      const url = "/scrap/" + tag;
+      await API.get(url);
+      enqueueSnackbar("Generate jobs success", { variant: "success" });
+    } catch (error) {
+      enqueueSnackbar("Generate jobs failed", { variant: "error" });
+    } finally {
+      setLoading(false);
+      setShowModalGenerateJobs(false);
+      setjobTagValue(null);
+      setParams({ filter: "all" });
+    }
+  };
+
+  const helperHandleClick = (type, row) => {
+    const dataForm =
+      type === "add"
+        ? {}
+        : {
+            ...row,
+            jobName: row.job_name,
+            workType: row.work_type,
+            jobTag: row.job_tag,
+            benefit: row?.benefit.toString(),
+          };
+    RHF.reset({ ...dataForm });
+    setTempRow(row);
+    setFormType(type);
+    setShowFormModal(true);
+  };
+  const handleCloseModalForm = () => {
+    setShowFormModal(false);
+    setFormType("");
+    setTempRow(null);
+    RHF.reset({});
+  };
+
+  const onSubmitForm = async () => {
+    const data = RHF.getValues();
+    if (formType === "add") {
+      handleAdd(data);
+    } else if (formType === "edit") {
+      handleEdit(data);
+    } else if (formType === "view") {
+      handleCloseModalForm();
+    }
+  };
+
+  //useeffect
+
+  useEffect(() => {
+    getData();
+  }, [params]);
+
+  const actions = {
+    view: true,
+    show: true,
+    add: true,
+    view: true,
+    delete: true,
+    edit: true,
+    download: true,
+    search: true,
+    filter: true,
+    customButton: [
+      {
+        show: true,
+        text: "Generate Jobs",
+        handleClick: () => setShowModalGenerateJobs(true),
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <TableCom
+        title="Job List"
+        actions={actions}
+        rows={dataRow}
+        columns={column}
+        pageSize={params.size}
+        pageNo={params.page}
+        total={total}
+        handleAdd={(row) => {
+          helperHandleClick("add", row);
+        }}
+        handleEdit={(row) => {
+          helperHandleClick("edit", row);
+        }}
+        handleDelete={(row) => {
+          setDeleteId(row.id);
+          setShowAlertDelete(true);
+        }}
+        handleView={(row) => {
+          helperHandleClick("view", row);
+        }}
+        filterProps={{
+          value: params.filter,
+          setValue: (val) => {
+            setParams((prev) => ({ ...prev, filter: val }));
+          },
+        }}
+        handleDownload={handleDownload}
+        loading={loading}
+      />
+      {showAlertDelete && (
+        <AlertCom
+          title={"Delete Data"}
+          loading={loading}
+          type="error"
+          width={{ xs: "80%", sm: "359px" }}
+          message={"Are you sure you want to delete this data?"}
+          open={showAlertDelete}
+          onClose={() => {
+            setShowAlertDelete(false);
+            setDeleteId(null);
+          }}
+          onConfirm={() => {
+            handleDelete(deleteId);
+          }}
+        />
+      )}
+      {showModalGenerateJobs && (
+        <ModalCom
+          open={showModalGenerateJobs}
+          onClose={() => {
+            setShowModalGenerateJobs(false);
+            setjobTagValue(null);
+          }}
+          loading={loading}
+          okText="Generate"
+          key="generateJobModal"
+          size="xs"
+          onConfirm={() => {
+            generateJobs();
+          }}
+          title="Generate Jobs Data"
+        >
+          <InputLabel id="jobtag" sx={{ mb: "20px" }}>
+            Please input job tag
+          </InputLabel>
+
+          <FormControl variant="standard" fullWidth>
+            <Select
+              labelId="generate-jobs-select-label"
+              id="generate-jobs-select"
+              value={jobTagValue}
+              onChange={(e) => setjobTagValue(e.target.value)}
+            >
+              {OptionsJobTag.map((val) => {
+                return <MenuItem value={val?.value}>{val?.label}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+        </ModalCom>
+      )}
+
+      {showFormModal && (
+        <ModalCom
+          open={showFormModal}
+          onClose={handleCloseModalForm}
+          loading={loading}
+          okText={formType === "view" ? "OK" : "Save"}
+          key="modalJobForm"
+          size="xl"
+          minHeight="65vh"
+          onConfirm={async () => {
+            const isValid = await RHF.trigger();
+            if (isValid) {
+              onSubmitForm();
+            }
+          }}
+          title={`${
+            formType.charAt(0).toUpperCase() + formType.slice(1)
+          } Job Data`}
+        >
+          <FormJob
+            RHF={RHF}
+            isViewOnly={formType === "view"}
+            formType={formType}
+            row={tempRow}
+          />
+        </ModalCom>
+      )}
+    </div>
+  );
+}
