f4f3e4229a8ce0f41adf9608e9d207c59a704ac7
Gerry Fernando <firmanaldif3@gmail.com>
2024-09-05 15:58:13 +0700

    Refactoring code and create router file

--- BE/index.js
+++ BE/index.js
@@ -1,34 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const Controller = require("./controller");
-const path = require("path");
+const app = require("./router");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//Give access to folder public
-app.use(express.static("public"));
-
-app.get("/ping", async (req, res) => {
-  res.status(200).send("pong");
-});
-
-app.get("/scrap/:jobTag", Controller.getScrapJobs);
-app.get("/download", Controller.downloadExcel);
-//Jobs
-app.get("/job", Controller.getJobs);
-app.post("/job", Controller.createJobs);
-app.put("/job/:id", Controller.editJobs);
-app.delete("/job/:id", Controller.deleteJobs);
-
-app.listen(PORT, (error) => {
+const server = app.listen(PORT, (error) => {
   if (!error)
     console.log(
       "Server is Successfully Running,   and App is listening on port " + PORT
     );
   else console.log("Error occurred, server can't start", error);
 });
+
+module.exports = server;
---
+++ BE/router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const Controller = require("./controller");
+const AuthController = require("./authController");
+// const authenticateJWT = require("./middleware");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Give access to folder public
+app.use(express.static("public"));
+
+app.get("/ping", async (req, res) => {
+  res.status(200).send("pong");
+});
+app.post("/login", AuthController.login);
+app.get("/scrap/:jobTag", Controller.getScrapJobs);
+app.get("/download", Controller.downloadExcel);
+//Jobs
+app.get("/job", Controller.getJobs);
+app.post("/job", Controller.createJobs);
+app.put("/job/:id", Controller.editJobs);
+app.delete("/job/:id", Controller.deleteJobs);
+
+module.exports = app;
